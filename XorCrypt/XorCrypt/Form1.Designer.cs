3eef5e84e1e2bb454afd7aa33d2c8ac7019f5cce
luiz-diniz <39447716+luiz-diniz@users.noreply.github.com>
2019-10-27 21:52:38 -0300

    Add files via upload

---
+++ XorCrypt/XorCrypt/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace XorCrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.btnKey = new System.Windows.Forms.Button();
+            this.btnMsg = new System.Windows.Forms.Button();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(166, 15);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(344, 20);
+            this.txtKey.TabIndex = 0;
+            // 
+            // btnKey
+            // 
+            this.btnKey.Location = new System.Drawing.Point(12, 12);
+            this.btnKey.Name = "btnKey";
+            this.btnKey.Size = new System.Drawing.Size(148, 23);
+            this.btnKey.TabIndex = 1;
+            this.btnKey.Text = "Gera a chave";
+            this.btnKey.UseVisualStyleBackColor = true;
+            this.btnKey.Click += new System.EventHandler(this.btnKey_Click);
+            // 
+            // btnMsg
+            // 
+            this.btnMsg.Location = new System.Drawing.Point(12, 72);
+            this.btnMsg.Name = "btnMsg";
+            this.btnMsg.Size = new System.Drawing.Size(148, 23);
+            this.btnMsg.TabIndex = 5;
+            this.btnMsg.Text = "Criptografia/Decript";
+            this.btnMsg.UseVisualStyleBackColor = true;
+            this.btnMsg.Click += new System.EventHandler(this.btnMsg_Click);
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(166, 72);
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(344, 20);
+            this.txtMsg.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 108);
+            this.Controls.Add(this.btnMsg);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.btnKey);
+            this.Controls.Add(this.txtKey);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "XorCrypt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Button btnKey;
+        private System.Windows.Forms.Button btnMsg;
+        private System.Windows.Forms.TextBox txtMsg;
+    }
+}
+
---
+++ XorCrypt/XorCrypt/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XorCrypt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKey_Click(object sender, EventArgs e)
+        {
+            //GERA UMA CHAVE RANDOM
+            //Cria uma lista de string
+            List<char> str = new List<char>();
+            Random random = new Random();
+            int i = 0;
+
+            while (i < 50)
+            {
+                //atribui um caracter random (ASCII do valor 50 à 100) na variável C
+                char c = (char)random.Next(50, 100);
+
+                // Adiciona o caracter (c) na lista de string
+                str.Add(c);
+                i++;
+            }
+            //Transforma o STR em um array que pode ser lido (chave final)
+            txtKey.Text = new string(str.ToArray());
+        }
+
+        private void btnMsg_Click(object sender, EventArgs e)
+        {
+            //CRIPTOGRAFA E DESCRIPTOGRAFA
+
+            //Verifica se a chave é maior que a mensagem
+            if (!(txtKey.Text.Length < txtMsg.Text.Length))
+            {
+                //Cria uma lista onde é será armazenado a mensagem criptografada
+                List<char> encryptedText = new List<char>();
+                int i = 0;
+
+                foreach(char c in txtMsg.Text)
+                {
+                    //usa xor de bit a bit do caracter da mensagem e da chave e armazena
+                    int character = (int)c ^ (int)txtKey.Text[i];
+
+                    //adiciona o char do inteiro "character"
+                    encryptedText.Add((char)character);
+                    i++;
+                }
+
+                //Mostra a mensagem criptografada/descriptografada
+                txtMsg.Text = new string(encryptedText.ToArray());
+            }
+            else
+            {
+                MessageBox.Show("The key must be larger than the message !","Fatal Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+    }
+}
