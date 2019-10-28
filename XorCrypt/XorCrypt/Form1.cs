26302130fad804f8fd47af12a0424d1837a12f15
luiz-diniz <39447716+luiz-diniz@users.noreply.github.com>
2019-10-28 13:35:06 -0300

    Add files via upload

--- XorCrypt/XorCrypt/Form1.cs
+++ XorCrypt/XorCrypt/Form1.cs
@@ -19,47 +19,33 @@ namespace XorCrypt
 
         private void btnKey_Click(object sender, EventArgs e)
         {
-            //GERA UMA CHAVE RANDOM
-            //Cria uma lista de string
-            List<char> str = new List<char>();
+            List<char> keyText = new List<char>();
             Random random = new Random();
             int i = 0;
 
-            while (i < 50)
+            for(i = 0;i < 50;i++)
             {
-                //atribui um caracter random (ASCII do valor 50 à 100) na variável C
                 char c = (char)random.Next(50, 100);
-
-                // Adiciona o caracter (c) na lista de string
-                str.Add(c);
-                i++;
+                keyText.Add(c);
             }
-            //Transforma o STR em um array que pode ser lido (chave final)
-            txtKey.Text = new string(str.ToArray());
+
+            txtKey.Text = new string(keyText.ToArray());
         }
 
         private void btnMsg_Click(object sender, EventArgs e)
         {
-            //CRIPTOGRAFA E DESCRIPTOGRAFA
-
-            //Verifica se a chave é maior que a mensagem
-            if (!(txtKey.Text.Length < txtMsg.Text.Length))
+            if (txtKey.Text.Length > txtMsg.Text.Length)
             {
-                //Cria uma lista onde é será armazenado a mensagem criptografada
                 List<char> encryptedText = new List<char>();
                 int i = 0;
 
                 foreach(char c in txtMsg.Text)
                 {
-                    //usa xor de bit a bit do caracter da mensagem e da chave e armazena
-                    int character = (int)c ^ (int)txtKey.Text[i];
-
-                    //adiciona o char do inteiro "character"
+                    int character = c ^ txtKey.Text[i];
                     encryptedText.Add((char)character);
                     i++;
                 }
 
-                //Mostra a mensagem criptografada/descriptografada
                 txtMsg.Text = new string(encryptedText.ToArray());
             }
             else
